c8ebb0255dd0e77f96552fa8a1f1fd0f443739a7
Dafa15 <dafaaldian25@gmail.com>
2023-12-26 20:09:06 +0700

    Fix target SDK to 31

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.dafa.suitmediatest"
-    compileSdk = 31
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.dafa.suitmediatest"
@@ -15,6 +15,9 @@ android {
         targetSdk = 31
         versionCode = 1
         versionName = "1.0"
+        multiDexEnabled = true
+
+
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -45,7 +48,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -54,8 +57,6 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation ("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 
